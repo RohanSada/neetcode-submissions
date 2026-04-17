32fd5cca4c0bfa2e3be00c7f29eafa2dbd6db34f
RohanSada <84402811+RohanSada@users.noreply.github.com>
2026-04-17 19:52:21 -0400

    Add: depth-of-binary-tree - submission-9

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-9.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''
+        # DFS
+        def traverse(node):
+            if not node:
+                return 0
+            left = traverse(node.left)
+            right = traverse(node.right)
+            return 1 + max(left, right)
+        return traverse(root)
+        '''
+
+        # BFS
+        if not root:
+            return 0
+        from collections import deque
+        queue = deque([root])
+        depth = 0
+        while len(queue)>0:
+            n = len(queue)
+            depth+=1
+            for _ in range(n):
+                node = queue.popleft()
+                for child in [node.left, node.right]:
+                    if child is None:
+                        continue
+                    queue.append(child)
+        return depth
+
